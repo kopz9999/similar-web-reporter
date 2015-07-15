79411a3a5048bdff5e7e3c5e794efbe1e49132d1
Kyoto Kopz <kopz9999@gmail.com>
2015-07-15 00:12:56 -0400

    Preparing project

---
+++ spec/code-spec.js
@@ -0,0 +1,11 @@
+var paks = require('../lib/word-document-handler');
+
+function helloWorld() {
+  return "Hello world!";
+}
+
+describe("Hello world", function() {
+  it("says hello", function() {
+    console.log(paks);
+  });
+});
